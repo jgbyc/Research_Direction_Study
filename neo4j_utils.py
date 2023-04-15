c66c03578567bb5402687474e63c8f58bf4ab86c
renkun007 <renkun007@gmail.com>
2023-04-14 23:26:59 -0700

    updated neo4j

--- neo4j_utils.py
+++ neo4j_utils.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 class neo4j_utils:
     def __init__(self) -> None:
-        self.driver = neo4j.GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "test_root"))
+        self.driver = neo4j.GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
     
     def top_faculty(self):
-        with self.driver.session() as session:
-            result = session.run("MATCH (f:Faculty) RETURN f.name AS name, f.numCitations AS numCitations ORDER BY numCitations DESC LIMIT 25")
+        with self.driver.session(database="academicworld") as session:
+            result = session.run("MATCH (f:FACULTY) RETURN f.name AS name, f.position AS position ORDER BY name LIMIT 25")
             df = pd.DataFrame(result.data())
             return df
