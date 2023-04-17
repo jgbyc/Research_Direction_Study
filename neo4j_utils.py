1fbb0268fe746491f41df74b5538dc16e21828d5
renkun007 <renkun007@gmail.com>
2023-04-16 20:42:56 -0700

    updated neo4j_utils

--- neo4j_utils.py
+++ neo4j_utils.py
@@ -4,9 +4,23 @@ import pandas as pd
 class neo4j_utils:
     def __init__(self) -> None:
         self.driver = neo4j.GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
-    
-    def top_faculty(self):
+        
+    def top_university(self, keyword_list):
+        query = """MATCH (K:KEYWORD)<--(P:PUBLICATION)<--(F:FACULTY)-->(I:INSTITUTE)
+                WHERE K.name IN $keywords 
+                RETURN I.name AS University, count(P) AS Publication_count 
+                ORDER BY Publication_count DESC 
+                LIMIT 10"""
         with self.driver.session(database="academicworld") as session:
-            result = session.run("MATCH (f:FACULTY) RETURN f.name AS name, f.position AS position ORDER BY name LIMIT 25")
+            result = session.run(query,keywords = keyword_list)
+            df = pd.DataFrame(result.data())
+            return df
+    def top_keywords(self):
+        query = """MATCH (K:KEYWORD)<--(P:PUBLICATION) 
+                RETURN K.name AS name, COUNT(P) AS keyword_count 
+                ORDER BY keyword_count DESC 
+                LIMIT 10"""
+        with self.driver.session(database="academicworld") as session:
+            result = session.run(query)
             df = pd.DataFrame(result.data())
             return df
